99d80f2b68d9c1816286489b84ce974da6d90775
Alex Mamonchik <alex.mamonchik@gmail.com>
2012-06-16 20:29:30 +0700

    added prefix and few methods

--- S3Component.php
+++ S3Component.php
@@ -22,7 +22,7 @@ class S3Component extends Component {
 	/**
 	 * @var string Object Name of the latest added object
 	 */
-	private $objectName;
+	private $prefix = '';
 
 	/**
 	 * @var string Amazon S3 Target Bucket
@@ -38,6 +38,7 @@ class S3Component extends Component {
 	public function initialize($controller) {
 
 		$this->S3Vendor = new S3(Configure::read('Amazon.key'), Configure::read('Amazon.secret'));
+		$this->prefix = Configure::read('Amazon.prefix');
 	}
 
 	
@@ -63,49 +64,79 @@ class S3Component extends Component {
 	 */
 	public function createFolder($name = '', $access = '') {
 		
-		$acc = '';
-		switch ($access) {
-			case 'prw':
-				$acc = S3::ACL_PUBLIC_READ_WRITE;
-				break;
-			case 'pr':
-				$acc = S3::ACL_PUBLIC_READ;
-				break;
-			case 'ar':
-				$acc = S3::ACL_AUTHENTICATED_READ;
-				break;
-			default :
-				$acc = S3::ACL_PRIVATE;
-		}
-		
-		return $this->S3Vendor->putBucket($name, $acc);
+		return $this->S3Vendor->putBucket($this->prefix.$name, $this->getPermissions($access));
 	}
 
 	
 	/**
 	 * Delete folder (Bucket)
-	 * @todo - make recursive deleting
 	 * @param string $name - name of folder for delete
+	 * @param boolean $recursive - will delete bucket with all files
 	 * @return type 
 	 */
 	public function deleteFolder($name = '', $recursive = false) {
 		
-		return $this->S3Vendor->deleteBucket($name);
+		if ($recursive) {
+			$files = $this->getFileList($name);
+			foreach ($files as $file) {
+				$this->deleteFile($this->prefix.$name, $file);
+			}
+		}
+		
+		return $this->S3Vendor->deleteBucket($this->prefix.$name);
+	}
+	
+	
+	/**
+	 * Get list of files in bucket
+	 * @param string $bucket - name of bucket
+	 * @return array - list of files in $bucket
+	 */
+	public function getFileList($bucket = '') {
+		
+		$bucket_info = $this->S3Vendor->getBucket($this->prefix.$bucket);
+
+		$files = array_keys($bucket_info);
+		
+		return $files;
 	}
 
 	
-	public function uploadFile($path_from = '', $to_bucket = '', $unique = false) {
+	/**
+	 * Upload file to Amazon s3
+	 * @param string $path_from - full path to file on local computer
+	 * @param string $to_bucket - name of bucket. Will be created If not exist
+	 * @param string $access - as in 'createFolder'
+	 * @param boolead $unique - save with original name or with unique random 20 length name
+	 * @return string | boolean - will return link to file if success upload and FALSE if else
+	 */
+	public function uploadFile($path_from = '', $to_bucket = '', $access = 'pr', $unique = false) {
 		
+		$isSuccess = false;
+		$filename = basename($path_from);
+
 		if ($unique) {
-			// save with unique name
+			$filename = $this->random();
 		}
+		
+		if (!$this->isExistFolder($to_bucket)) {
+			$this->createFolder($this->prefix.$bucket, 'pr');
+		}
+		
+		if ($this->S3Vendor->putObjectFile($path_from, $this->prefix.$to_bucket, $filename, $this->getPermissions($access))) {
+			$isSuccess = $this->prefix.$to_bucket.'/'.$filename;
+		}
+		
+		return $isSuccess;
 	}
 
-	public function getFile($filename = '', $save_to = null) {
+	public function getFile($bucket ='', $filename = '', $save_to = null) {
+		
 		
 	}
 
-	public function deleteFile($filename = '') {
+	public function deleteFile($bucket ='', $filename = '') {
+		
 		
 	}
 
@@ -124,4 +155,41 @@ class S3Component extends Component {
 		return $string;
 	}
 
+	
+	private function getPermissions($access) {
+		
+		$acc = '';
+		switch ($access) {
+			case 'prw':
+				$acc = S3::ACL_PUBLIC_READ_WRITE;
+				break;
+			case 'pr':
+				$acc = S3::ACL_PUBLIC_READ;
+				break;
+			case 'ar':
+				$acc = S3::ACL_AUTHENTICATED_READ;
+				break;
+			default :
+				$acc = S3::ACL_PRIVATE;
+		}
+		
+		return $acc;
+	}
+	
+	
+	/**
+	 * Check exist bucket in Amazon S3
+	 * @param string $bucket - nam of bucket
+	 * @return boolean - true if $bucket already exist 
+	 */
+	private function isExistFolder($bucket = '') {
+		
+		$isExist = true;
+		
+		if ($this->S3Vendor->getBucket($this->prefix.$bucket) === false) {
+			$isExist = false;
+		}
+		
+		return $isExist;
+	}
 }
